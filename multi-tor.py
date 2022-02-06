35bb8b16c0f23f0680e07b29a6d1ddebe29cff98
rfc01 <97000835+rfc01@users.noreply.github.com>
2022-02-06 21:41:09 +0000

    Update multi-tor.py
    First update for functions creation.
    It is now possible to import the multi-tor functions to other python scripts

--- multi-tor.py
+++ multi-tor.py
@@ -1,29 +1,48 @@
 import os
 import requests
 import subprocess
-import _thread
 import argparse
 import glob
 import shutil
 import json
 
+def create_tor_env(port):
+    path = str(os.getcwd())
+    #create tor data sirectory
+    try:
+        os.mkdir(path + "/datadir")
+        os.mkdir(path + "/torrc")
+        os.mkdir(path + "/datadir/datadir" + str(port))
+    except:
+        pass
+    
+    #create torrc files
+    filename = "torrc/torrc." + str(port)
+    f = open(filename, "w+")
+    f.write("SocksPort " + str(port) + "\n\r")
+    f.write("ControlPort " + str(port + 100) + "\n\r")
+    f.write("DataDirectory ./datadir/datadir" + str(port))
+    f.close()
 
-DEFAULT_PORT = 9050
-TOR_PATH = '"Tor/tor.exe"'
+def launch_tor(port):
+    path = str(os.getcwd())
+    cmd = TOR_PATH + ' -f ' + path + '/torrc/torrc.' + str(port) + ' | more'
+    sp = subprocess.Popen(cmd, stdout=subprocess.PIPE, shell=True)
 
-# Defines and Global Variables
-parser = argparse.ArgumentParser(description='Tor Relay Launcher')
-parser.add_argument('-c', '--clean', help='clean tor workspace', dest='clean', action='store_true', required=False)
-parser.add_argument('-n', '--ninst', type=int, help='number of Tor instances', required=False)
-parser.add_argument('-p', '--ports', type=int, help='initial port', required=False)
+def validate_tor_relay(port):
+    proxies = {
+        'http': 'socks5://127.0.0.1:'+str(port),
+        'https': 'socks5://127.0.0.1:' + str(port)
+    }
+    while True:
+        try:
+            exit_ip = requests.get('https://ipinfo.io/', proxies=proxies).json()
+            print('Launched Tor on 127.0.0.1:'+str(port),'|', 'exit node:',exit_ip['ip'],'\t',exit_ip['region'],'-',exit_ip['country'])
+            break
+        except:
+            pass
 
-parser.set_defaults(clean=False, ninst=0, ports=DEFAULT_PORT)
-
-# Parse command-line arguments
-args = parser.parse_args()
-
-# Remove old workspace
-if args.clean:
+def clean_tor_relays():
 
     # Kill running tor processes
     os.system("taskkill /f /im  tor.exe")
@@ -53,60 +72,51 @@ if args.clean:
         os.remove(filePath)
     except:
         print("Error while deleting file : ", filePath)
+
+
+
+DEFAULT_PORT = 9050
+TOR_PATH = '"Tor/tor.exe"'
+
+# Defines and Global Variables
+parser = argparse.ArgumentParser(description='Tor Relay Launcher')
+parser.add_argument('-c', '--clean', help='clean tor workspace', dest='clean', action='store_true', required=False)
+parser.add_argument('-n', '--ninst', type=int, help='number of Tor instances', required=False)
+parser.add_argument('-p', '--ports', type=int, help='initial port', required=False)
+parser.add_argument('-l', '--port_list', help='create ip:port access list', dest='port_list', action='store_true', required=False)
+
+parser.set_defaults(clean=False, ninst=0, ports=DEFAULT_PORT, port_list=False)
+
+# Parse command-line arguments
+args = parser.parse_args()
+
+# Remove old workspace
+if args.clean:
+    clean_tor_relays()
     
-
-
-if args.ninst == 0:
+if args.ninst <= 0:
     exit(1)
+else:
 
-
-
-print("Create DataDirectory")
-print("Creating torrc files")
-for port in range(args.ports,args.ports+args.ninst):
-
-    path = str(os.getcwd())
-    try:
-        os.mkdir(path + "/datadir")
-        os.mkdir(path + "/torrc")
-        os.mkdir(path + "/datadir/datadir" + str(port))
-    except:
-        pass
+    print("Create DataDirectory and torrc files... ", end = '', flush=True)    
+    for port in range(args.ports,args.ports+args.ninst):
+        create_tor_env(port)
+    print("Done")
     
-    #create torrc files
-    
-    filename = "torrc/torrc." + str(port)
+    # launch all tor relay circuits
+    print("Launching Tor nodes, this make take a few seconds. ")
+    for port in range(args.ports,args.ports+args.ninst):    
+        launch_tor(port)
+        
+    # Check and validate IPs and location for all tor circuits
+    for port in range(args.ports,args.ports+args.ninst):    
+        validate_tor_relay(port)
+
+if args.port_list:
+    # Create tor relay list
+    filename = "torrelay.list"
     f = open(filename, "w")
-    f.write("SocksPort " + str(port) + "\n\r")
-    f.write("ControlPort " + str(port + 100) + "\n\r")
-    f.write("DataDirectory ./datadir/datadir" + str(port))
+    for port in range(args.ports,args.ports+args.ninst):
+        f.write(str(port)+'\n')
     f.close()
-    
-print("Launching Tor nodes...")
-
-for port in range(args.ports,args.ports+args.ninst):    
-
-    cmd = TOR_PATH + ' -f ' + path + '/torrc/torrc.' + str(port) + ' | more'
-    sp = subprocess.Popen(cmd, stdout=subprocess.PIPE, shell=True)
-
-
-for port in range(args.ports,args.ports+args.ninst):    
-    proxies = {
-        'http': 'socks5://127.0.0.1:'+str(port),
-        'https': 'socks5://127.0.0.1:' + str(port)
-    }
-    while True:
-        try:
-            exit_ip = requests.get('https://ipinfo.io/', proxies=proxies).json()
-            print('Launched Tor on 127.0.0.1:'+str(port),'|', 'exit node:',exit_ip['ip'],'\t',exit_ip['region'],'-',exit_ip['country'])
-            break
-        except:
-            pass
-
-
-# Create tor relay list
-filename = "torrelay.list"
-f = open(filename, "w")
-for port in range(args.ports,args.ports+args.ninst):
-    f.write(str(port))
-f.close()
+    print("Access port list created:", filename)
